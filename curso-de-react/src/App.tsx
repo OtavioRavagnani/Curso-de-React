6754d4cf8bd15a29580d6757eb8a16f6079fdfc5
OtavioRavagnani <otavioravagnani01@gmail.com>
2024-01-02 18:08:47 -0300

    React router Dom adicionado

--- curso-de-react/src/App.tsx
+++ curso-de-react/src/App.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
-  return <div className="App">teste</div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 };
