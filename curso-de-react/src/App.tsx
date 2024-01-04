e091ec8d3eaacaf667a6519b49c1c32ed696f175
OtavioRavagnani <otavioravagnani01@gmail.com>
2024-01-04 17:24:22 -0300

    Mui Configurado

--- curso-de-react/src/App.tsx
+++ curso-de-react/src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@emotion/react";
+import { LitghtTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LitghtTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
--- curso-de-react/src/routes/index.tsx
+++ curso-de-react/src/routes/index.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Pagina Inicial</p>} />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary">
+            Teste
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
