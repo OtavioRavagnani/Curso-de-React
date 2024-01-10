55239f55b3312167688704cd18cc12b4d16b0408
OtavioRavagnani <otavioravagnani01@gmail.com>
2024-01-10 11:15:01 -0300

    fix: side bar pronta

--- curso-de-react/src/shared/components/menu-lateral/Menu-Lateral.tsx
+++ curso-de-react/src/shared/components/menu-lateral/Menu-Lateral.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Drawer, useTheme } from '@mui/material';
+import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
 
 interface IAppThemeProviderProps {
   children: React.ReactNode;
@@ -10,12 +10,27 @@ export const MenuLateral: React.FC<IAppThemeProviderProps> = ({ children }) => {
   return (
     <>
       <Drawer variant='permanent'>
-        <Box width={theme.spacing(28)}>
+        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+
           <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center" >
             <Avatar
-              sx={{}}
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
               src="https://scontent.frao2-1.fna.fbcdn.net/v/t39.30808-1/297526917_3229049594079658_8998610346560140117_n.jpg?stp=cp0_dst-jpg_p60x60&_nc_cat=102&ccb=1-7&_nc_sid=5740b7&_nc_eui2=AeEkjBPC9BAc-7IaVnfIQH_yKG61cP40pIEobrVw_jSkgXb6Ov0Y6BdJAfr34G5Rwq6L3AJF2cPRZ3A40px6dOTB&_nc_ohc=HrBD7Bs2JUIAX_A9_mQ&_nc_ht=scontent.frao2-1.fna&oh=00_AfAdulSOnUOLSEg833E5Cff7_ur64KybUEJavIKYzGC1-w&oe=65A2759C" />
           </Box>
+
+          <Divider />
+
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Pagina Inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+
         </Box >
       </Drawer>
 
