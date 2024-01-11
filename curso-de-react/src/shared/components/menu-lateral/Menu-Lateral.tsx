d468005cf02c79f87da2063e417eed0a00488bd7
OtavioRavagnani <otavioravagnani01@gmail.com>
2024-01-11 17:56:06 -0300

    fix: Drawer select PRONTO

--- curso-de-react/src/shared/components/menu-lateral/Menu-Lateral.tsx
+++ curso-de-react/src/shared/components/menu-lateral/Menu-Lateral.tsx
@@ -1,4 +1,5 @@
 import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
+import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
 import { Box } from '@mui/system';
 
 import { useDrawerContext } from '../../contexts';
@@ -7,11 +8,40 @@ interface IAppThemeProviderProps {
   children: React.ReactNode;
 }
 
-export const MenuLateral: React.FC<IAppThemeProviderProps> = ({ children }) => {
+interface IListItemLinkProps {
+  to: string;
+  icon: string;
+  label: string;
+  onClick: (() => void) | undefined;
+}
+const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }) => {
+  const navigate = useNavigate();
+
+  const resolvedPath = useResolvedPath(to);
+  const match = useMatch({ path: resolvedPath.pathname, end: false });
+
+
+  const handleClick = () => {
+    navigate(to);
+    onClick?.();
+  };
+
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick}>
+      <ListItemIcon>
+        <Icon>{icon}</Icon>
+      </ListItemIcon>
+      <ListItemText primary={label} />
+    </ListItemButton>
+  );
+};
+
+
+export const MenuLateral: React.FC<IAppThemeProviderProps>= ({ children }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const { isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
+  const { isDrawerOpen, drawerOptions, toggleDrawerOpen } = useDrawerContext();
 
   return (
     <>
@@ -29,12 +59,15 @@ export const MenuLateral: React.FC<IAppThemeProviderProps> = ({ children }) => {
 
           <Box flex={1}>
             <List component="nav">
-              <ListItemButton>
-                <ListItemIcon>
-                  <Icon>home</Icon>
-                </ListItemIcon>
-                <ListItemText primary="Página inicial" />
-              </ListItemButton>
+              {drawerOptions.map(drawerOption => (
+                <ListItemLink
+                  to={drawerOption.path}
+                  key={drawerOption.path}
+                  icon={drawerOption.icon}
+                  label={drawerOption.label}
+                  onClick={smDown ? toggleDrawerOpen : undefined}
+                />
+              ))}
             </List>
           </Box>
 
--- curso-de-react/src/shared/contexts/DrawerContext.tsx
+++ curso-de-react/src/shared/contexts/DrawerContext.tsx
@@ -1,14 +1,21 @@
 import { createContext, useCallback, useContext, useState } from 'react';
 
-interface IDrawerContextData {
-  isDrawerOpen: boolean;
-  toggleDrawerOpen: () => void;
+interface IDrawerOption {
+  icon: string;
+  path: string;
+  label: string;
 }
 
 interface IAppThemeProviderProps {
   children: React.ReactNode;
 }
 
+interface IDrawerContextData {
+  isDrawerOpen: boolean;
+  toggleDrawerOpen: () => void;
+  drawerOptions: IDrawerOption[];
+  setDrawerOptions: (newDrawerOptions: IDrawerOption[]) => void;
+}
 
 const DrawerContext = createContext({} as IDrawerContextData);
 
@@ -17,14 +24,19 @@ export const useDrawerContext = () => {
 };
 
 export const DrawerProvider: React.FC<IAppThemeProviderProps> = ({ children }) => {
+  const [drawerOptions, setDrawerOptions] = useState<IDrawerOption[]>([]);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const toggleDrawerOpen = useCallback(() => {
     setIsDrawerOpen(oldDrawerOpen => !oldDrawerOpen);
   }, []);
 
+  const handleSetDrawerOptions = useCallback((newDrawerOptions: IDrawerOption[]) => {
+    setDrawerOptions(newDrawerOptions);
+  }, []);
+
   return (
-    <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen }}>
+    <DrawerContext.Provider value={{ isDrawerOpen, drawerOptions, toggleDrawerOpen, setDrawerOptions: handleSetDrawerOptions }}>
       {children}
     </DrawerContext.Provider>
   );
