9d9ad02bdd5cdf4fb570d216057c0b1e6f1225d2
OtavioRavagnani <otavioravagnani01@gmail.com>
2024-01-17 11:03:44 -0300

    teste

--- curso-de-react/src/shared/components/menu-lateral/Menu-Lateral.tsx
+++ curso-de-react/src/shared/components/menu-lateral/Menu-Lateral.tsx
@@ -2,7 +2,7 @@ import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, List
 import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
 import { Box } from '@mui/system';
 
-import { useDrawerContext } from '../../contexts';
+import { useAppThemeContext, useDrawerContext } from '../../contexts';
 
 interface IAppThemeProviderProps {
   children: React.ReactNode;
@@ -37,11 +37,12 @@ const ListItemLink: React.FC<IListItemLinkProps> = ({ to, icon, label, onClick }
 };
 
 
-export const MenuLateral: React.FC<IAppThemeProviderProps>= ({ children }) => {
+export const MenuLateral: React.FC<IAppThemeProviderProps> = ({ children }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
   const { isDrawerOpen, drawerOptions, toggleDrawerOpen } = useDrawerContext();
+  const { toggleTheme } = useAppThemeContext();
 
   return (
     <>
@@ -71,6 +72,16 @@ export const MenuLateral: React.FC<IAppThemeProviderProps>= ({ children }) => {
             </List>
           </Box>
 
+          <Box>
+            <List component="nav">
+              <ListItemButton onClick={toggleTheme}>
+                <ListItemIcon>
+                  <Icon>dark_mode</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Alternar tema" />
+              </ListItemButton>
+            </List>
+          </Box>
         </Box>
       </Drawer>
 
