cb0332718d94893a980557181021cda85102907a
OtavioRavagnani <otavioravagnani01@gmail.com>
2024-01-17 11:09:32 -0300

    fix: dark theme configurado

--- curso-de-react/src/shared/themes/Dark.ts
+++ curso-de-react/src/shared/themes/Dark.ts
@@ -3,21 +3,27 @@ import { cyan, yellow } from '@mui/material/colors';
 
 export const DarkTheme = createTheme({
   palette: {
+    mode: 'dark',
     primary: {
       main: yellow[700],
       dark: yellow[800],
       light: yellow[500],
-      contrastText: '#fffff',
+      contrastText: '#ffffff',
     },
     secondary: {
       main: cyan[500],
       dark: cyan[400],
       light: cyan[300],
-      contrastText: '#fffff',
+      contrastText: '#ffffff',
     },
     background: {
-      default: '#303134',
-      paper: '#202124',
+      paper: '#303134',
+      default: '#202124',
     },
   },
+  typography: {
+    allVariants: {
+      color: 'white',
+    }
+  }
 });
