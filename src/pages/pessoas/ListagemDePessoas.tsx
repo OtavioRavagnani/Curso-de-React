7ea1634a9dbfd79cb2dfdc3a1f44a7a5af03761f
OtavioRavagnani <otavioravagnani01@gmail.com>
2024-02-08 21:23:27 -0300

    feat: pesquisa backend pronta

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,20 +1,30 @@
 /* eslint-disable react/no-children-prop */
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import { FerramentasDaListagem } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 
-export const ListagemDeCidade: React.FC = () => {
+export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const busca = useMemo(() => {
     return searchParams.get("busca") || "";
   }, [searchParams]);
 
+  useEffect(() => {
+    PessoasService.getAll(1, busca).then((result) => {
+      if (result instanceof Error) {
+        alert(result.message);
+        return;
+      }
+    });
+  }, [busca]);
+
   return (
     <LayoutBaseDePagina
-      titulo="Listagem de cidades"
+      titulo="Listagem de pessoas"
       barraDeFerramentas={
         <FerramentasDaListagem
           mostrarInputBusca
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import { useDrawerContext } from "../shared/contexts";
-import { Dashboard, ListagemDeCidade } from "../pages";
+import { Dashboard, ListagemDePessoas } from "../pages";
 
 export const AppRoutes = () => {
   const { setDrawerOptions } = useDrawerContext();
@@ -15,9 +15,9 @@ export const AppRoutes = () => {
         label: "Página inicial",
       },
       {
-        icon: "location_city",
-        path: "/cidades",
-        label: "Cidades",
+        icon: "people",
+        path: "/pessoas",
+        label: "Pessoas",
       },
     ]);
   }, []);
@@ -26,8 +26,8 @@ export const AppRoutes = () => {
     <Routes>
       <Route path="/pagina-inicial" element={<Dashboard />} />
 
-      <Route path="/cidades" element={<ListagemDeCidade />} />
-      {/* <Route path="/cidades/detalhe/:id" element={<Dashboard />} /> */}
+      <Route path="/pessoas" element={<ListagemDePessoas />} />
+      {/* <Route path="/pessoas/detalhe/:id" element={<Dashboard />} /> */}
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
